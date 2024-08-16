cfac713f19607fd89f17b5dc6c7f2d8e6c0b6df8
Rachi08 <jain.rachii@gmail.com>
2024-08-16 22:10:22 +0530

    start

---
+++ src/views/Tailwind-Landing-Page/Layout.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+import Navbar from './components/Navbar/Navbar';
+import '.App.css';
+const Layout = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Layout
